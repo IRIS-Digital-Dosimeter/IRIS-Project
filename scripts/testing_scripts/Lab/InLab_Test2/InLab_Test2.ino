1d10dea86cfef1b8ab26dc8f525bf23900e81e65
Drixitel <mpmunoz1993@gmail.com>
2023-08-07 13:59:00 -0700

    cleaned lab test 2

--- scripts/testing_scripts/Lab/InLab_Test2/InLab_Test2.ino
+++ scripts/testing_scripts/Lab/InLab_Test2/InLab_Test2.ino
@@ -79,7 +79,7 @@ void setup(){
   // Set up Serial Monitor communication  
   SPI_initialization(baudRate);
   // Set up SD card 
-
+  SD_initialization(chipSelect);
   // Set the analog pin resolution 
   analogReadResolution(12);
   // Ask for the date: MM/DD
@@ -87,11 +87,10 @@ void setup(){
   // Ask for the desired file (time) length  
   extractIntervalFromInput();
 
-  usb_initialization();
 
 
 
-  SD_initialization(chipSelect);
+
 
 }
 
@@ -105,59 +104,4 @@ void loop() {
 
 }
 
-void usb_initialization() {
-  // Vendor ID, Product ID, Product Rev
-  usb_msc.setID("Adafruit", "SD Card", "1.0");
-  // lookup call back functions      
-  usb_msc.setReadWriteCallback(msc_read_cb, msc_write_cb, msc_flush_cb);
-  usb_msc.setUnitReady(false); 
-  usb_msc.begin();
-  
-  Serial.println("Adafruit TinyUSB Mass Storage SD Card example");
 
-  Serial.println("\nInitializing SD card...");
-  if (!card.init(SPI_HALF_SPEED, chipSelect)) // if not run the checks
-  {
-    Serial.println("initialization failed. Things to check:");
-    Serial.println("* is a card inserted?");
-    Serial.println("* is your wiring correct?");
-    Serial.println("* did you change the chipSelect pin to match your shield or module?");
-    while (1)
-      delay(1);
-  }
-  if (!volume.init(card)) // attepts to open the Fat16 or Fat 32
-  {
-    Serial.println("Could not find FAT16/FAT32 partition. \nMake sure you've formatted the card");
-    while (1)
-      delay(1);
-  }
-
-  uint32_t block_count = volume.blocksPerCluster() * volume.clusterCount();
-  // Checks
-  Serial.print("Volume size (MB): ");
-  Serial.println((block_count, 512));
-
-  usb_msc.setCapacity(block_count, 512);
-  usb_msc.setUnitReady(true); // now the mass storage is ready to read/write
-
-}
-
-// function callback for external usb --------------------------------------------------------
-int32_t msc_read_cb(uint32_t lba, void *buffer, uint32_t bufsize)
-{
-  (void)bufsize;
-  return card.readBlock(lba, (uint8_t *)buffer) ? 512 : -1;
-}
-
-// function callback
-int32_t msc_write_cb(uint32_t lba, uint8_t *buffer, uint32_t bufsize)
-{
-  (void)bufsize;
-  return card.writeBlock(lba, buffer) ? 512 : -1;
-}
-
-// function callback
-void msc_flush_cb(void)
-{
-  // should flush we're just going to have it as a place holder
-}
