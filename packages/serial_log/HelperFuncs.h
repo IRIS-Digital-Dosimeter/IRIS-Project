86e95c198c74c5c1a6483df68ab02fd20b685969
Drixitel <mpmunoz1993@gmail.com>
2024-05-05 19:04:06 -0700

    update made to serial log

--- packages/serial_log/HelperFuncs.h
+++ packages/serial_log/HelperFuncs.h
@@ -1,14 +1,17 @@
 // HelperFuncs.h
+#ifndef HELPER_FUNC_H
+#define HELPER_FUNC_H
 
-extern uint32_t session_val;
-extern uint32_t desiredInterval_s;
-extern uint32_t desiredInterval_ms;
-extern uint32_t desiredInterval_us;
 
 /* Fast Board */
 extern uint32_t interSampleDelay; 
 extern uint32_t interAverageDelay; 
 extern uint32_t numSamples; 
 
-
 void myDelay_us(uint32_t us);
+namespace Setup {
+    void SPI_init(const uint32_t baudRate);
+}
+
+
+#endif
--- packages/serial_log/serial_log.ino
+++ packages/serial_log/serial_log.ino
@@ -4,52 +4,82 @@
 // Github: https://github.com/aidanzinn/Adafruit_Constant_Log/blob/master/import_serial.py
 // Works on M0 and M4
 
+// Update: 5/5/24
+// by: Michelle P
+
 #include "HelperFuncs.h"
 
-uint32_t startAnalogTimer = 0;
+// LED 
+#define REDLEDpin 13 
+int ledState = LOW;
+unsigned long previousMillis = 0;  // will store last time LED was updated
+// 1 min blinks
+const long interval = 60000;  // interval at which to blink (milliseconds)
+
+
+struct datastore {
+  uint32_t t1;
+  uint32_t t2;
+  uint32_t adc0; 
+  uint32_t adc1; 
+};
+
+struct datastore myData;
+
 
 void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(500000);
+
+  // Set the analog pin resolution 
+  analogReadResolution(12);
+
+  // Turn LED off
+  pinMode(REDLEDpin, OUTPUT);
+  digitalWrite(REDLEDpin, LOW);
+
+  // Set input pins
+  pinMode(A0, INPUT);
+  pinMode(A1, INPUT);
+
+ Setup::SPI_init(115200);
 
 }
 
 void loop() {
 
+
+  // Store inital values/clear previous values
+  myData.adc0 = 0; 
+  myData.adc1 = 0; 
+
   // Store start Time
-  // startAnalogTimer = micros();
-
-  // Declare local variable/Buffer 
-  uint32_t sum_sensorValue0 = 0; 
-  uint32_t sum_sensorValue1 = 0;
-
-  uint32_t timeBefore = micros();
+  myData.t1 = micros();
 
   // Build buffer: read sensor value then sum it to the previous sensor value 
   for (uint32_t counter = 1; counter <= numSamples; counter++){
-    sum_sensorValue0 += analogRead(A0);
-    sum_sensorValue1 += analogRead(A1);
+    myData.adc0 += analogRead(A0);
+    myData.adc1 += analogRead(A1);
     // Pause for stability 
-    // myDelay_us(interSampleDelay);
+    myDelay_us(interSampleDelay);
   }
-  uint32_t timeAfter = micros();
+  myData.t2 = micros();
 
-  // uint32_t timesum = timeBefore + timeAfter;
   // Pause for stability 
   // myDelay_us(interAverageDelay);
 
-  // Send the summed data via Serial
-  Serial.print(timeBefore);
-  Serial.print(',');
-  Serial.print(timeAfter);
-  Serial.print(',');
-  Serial.print(sum_sensorValue0);
-  Serial.print(',');
-  Serial.println(sum_sensorValue1);
+  Serial.write((uint8_t*)&myData, sizeof(myData));
+
+  // FLASH LED -----------------------------------------
+  unsigned long currentMillis = millis();
+  if (currentMillis - previousMillis >= interval) {
+    // save the last time you blinked the LED
+    previousMillis = currentMillis; 
+    // Toggle the LED state
+    ledState = !ledState;
+    // set the LED with the ledState of the variable:
+    digitalWrite(REDLEDpin, ledState);
+  }
 
 
-  // Optional: Reset startAnalogTimer if you want to keep sending data at regular intervals
-  // startAnalogTimer = micros();
    
 }
 
