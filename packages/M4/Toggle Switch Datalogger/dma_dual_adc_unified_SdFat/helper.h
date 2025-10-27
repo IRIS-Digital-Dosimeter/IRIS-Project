8a6951433a5ce373e8732c75216feec4f4efd97b
Baron-Paelen <50848243+Baron-Paelen@users.noreply.github.com>
2025-10-26 20:05:03 -0700

    initial commit for MDA sketch with toggle switch. needs in-lab testing to see if my home testing is just flawed

---
+++ packages/M4/Toggle Switch Datalogger/dma_dual_adc_unified_SdFat/helper.h
@@ -0,0 +1,31 @@
+#include "samd51/include/samd51j19a.h"
+#include <cstddef>
+#include <stdint.h>
+#include <SdFat.h>
+// #include "config.h"
+
+#pragma once
+
+typedef struct // DMAC descriptor struct
+{
+    uint16_t btctrl;
+    uint16_t btcnt;
+    uint32_t srcaddr;
+    uint32_t dstaddr;
+    uint32_t descaddr;
+} dmadesc;
+
+void ADCsync();
+void adc_init();
+void dma_init();
+void dma_channels_enable();
+
+bool create_dat_file(SdFs* sd, FsFile* file);
+bool do_rollover_if_needed(SdFs* sd, FsFile* file, size_t size);
+
+void print_binary(uint32_t regValue);
+bool find_largest_file_number(SdFs* sd, const char* extension);
+int get_new_file_number();
+
+// void DMAC_2_Handler();
+// void DMAC_3_Handler();
